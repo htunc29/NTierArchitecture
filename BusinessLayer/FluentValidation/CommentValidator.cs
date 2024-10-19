ad6e183bf1a4f7de823f1a95944c35e7be41ac98
Hüseyin Tunç <huseyint428@gmail.com>
2024-10-19 20:49:53 +0300

    fluent validation comment için eklendi

---
+++ BusinessLayer/FluentValidation/CommentValidator.cs
@@ -0,0 +1,24 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.FluentValidation
+{
+    public class CommentValidator:AbstractValidator<Comment>
+    {
+        public CommentValidator()
+        { 
+        RuleFor(x => x.CommentContent).NotEmpty().WithMessage("Comment Content cannot be empty");
+            RuleFor(x => x.CommentContent).MinimumLength(3).WithMessage("Comment Content must be at least 3 characters");
+            RuleFor(x => x.CommentContent).MaximumLength(100).WithMessage("Comment Content must be at most 100 characters");
+            RuleFor(x => x.CommentDate).NotEmpty().WithMessage("Comment Date cannot be empty");
+            RuleFor(x=>x.ProductID).NotEmpty().WithMessage("Product ID cannot be empty");
+            RuleFor(x=>x.CustomerID).NotEmpty().WithMessage("Customer ID cannot be empty");
+
+        }
+    }
+}
--- UILayer/Program.cs
+++ UILayer/Program.cs
@@ -1,6 +1,8 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.FluentValidation;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
+using FluentValidation.Results;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -36,19 +38,34 @@ namespace UILayer
                 CustomerMail = customerMail,
                 CustomerPassword = customerPassword
             };
-            customerManager.TInsert(customer);
-            Console.WriteLine("Customer Added");
-            Console.WriteLine("--------------------");
-            Console.WriteLine("Customer List");
-            foreach (var item in customerManager.TList())
+            CustomerValidator customerValidator = new CustomerValidator();
+            ValidationResult result = customerValidator.Validate(customer);
+            if(result.IsValid)
             {
-                Console.WriteLine("Customer Name:" + item.CustomerName);
-                Console.WriteLine("Customer Surname:" + item.CustomerSurname);
-                Console.WriteLine("Customer Adress:" + item.CustomerAdress);
-                Console.WriteLine("Customer Mail:" + item.CustomerMail);
-                Console.WriteLine("Customer Password:" + item.CustomerPassword);
+                customerManager.TInsert(customer);
+                Console.WriteLine("Customer Added");
                 Console.WriteLine("--------------------");
+                Console.WriteLine("Customer List");
+                foreach (var item in customerManager.TList())
+                {
+                    Console.WriteLine("Customer Name:" + item.CustomerName);
+                    Console.WriteLine("Customer Surname:" + item.CustomerSurname);
+                    Console.WriteLine("Customer Adress:" + item.CustomerAdress);
+                    Console.WriteLine("Customer Mail:" + item.CustomerMail);
+                    Console.WriteLine("Customer Password:" + item.CustomerPassword);
+                    Console.WriteLine("--------------------");
+                }
             }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    Console.WriteLine(item.ErrorMessage);
+                }
+            }
+
+            Console.WriteLine("--------------------");
+
 
             Console.ReadLine();
         }
